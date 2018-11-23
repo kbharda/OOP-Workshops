b9ce008fe8d7cf17effcbebd85147d6c8f74058e
Kashyap Bharda <kashyapbharda12645@gmail.com>
2018-11-22 22:26:42 -0500

    ws9

--- WS09/in_lab/Data.cpp
+++ WS09/in_lab/Data.cpp
@@ -1,4 +1,12 @@
 
+// Workshop 9 :Function Templates 
+// Name: Kashyap Bharda	
+// Student #: 140622176
+// Course: OOP244 Fall 2018
+// File: Data.cpp
+
+#include "Data.h"
+using namespace std;
 
 namespace sict {
 	// readRow extracts a single record from input and stores n data fields in data
@@ -80,25 +88,26 @@ namespace sict {
 		// e..g "6.31 million";
 		// note that population is an integer, so dividing by 1000000 would yield "6"
 
-
-
+		std::cout.setf(std::ios::fixed);
+		std::cout.precision(2);
+		std::cout << "Population change from " << year[0] << " to " << year[n - 1] << " is " << static_cast<double>(population[n - 1] - population[0]) / 1000000 << " million " << std::endl;
 
 
 		// Q2. print whether violent crime rate has gone up or down between 2000 and 2005
 
-
-
-
+		std::cout << "Violet Crime trend is " << (violentCrimeRate[n - 1] < violentCrimeRate[0] ? "down" : "up") << std::endl;
 
 
 		// Q3 print the GTA number accurate to 0 decimal places
 
-
+		std::cout << "There are " << static_cast<double>(average(grandTheftAuto, n)) / 1000000 << " million Grand Theft Auto incidents on average a year" << endl;
 
 
 		// Q4. Print the min and max violentCrime rates
 
+		std::cout << "The Minimun Violent Crime rate was " << static_cast<int>(min(violentCrimeRate, n)) << endl;
+		std::cout << "The Maximum Violent Crime rate was " << static_cast<int>(max(violentCrimeRate, n)) << endl;
 
-
+		std::cout.unsetf(std::ios::fixed);
 	}
 }
--- WS09/in_lab/Data.h
+++ WS09/in_lab/Data.h
@@ -1,6 +1,18 @@
+#pragma once
 
+// Workshop 9 :Function Templates 
+// Name: Kashyap Bharda	
+// Student #: 140622176
+// Course: OOP244 Fall 2018
+// File: Data.h
 
+#ifndef SICT_DATA_H
+#define SICT_DATA_H
 
+#include <iostream>
+#include <cstring>
+
+using namespace std;
 
 namespace sict {
 	// These numbers define 
@@ -9,23 +21,74 @@ namespace sict {
 	//    use them in the min and max function
 	// b) the output column width for a single data field
 
-	const int LARGEST_NUMBER  = 1000000000; // 1,000,000,000
-	const int SMALLEST_NUMBER = 0;          
-	const int COLUMN_WIDTH    = 15;
+	const int LARGEST_NUMBER = 1000000000; // 1,000,000,000
+	const int SMALLEST_NUMBER = 0;
+	const int COLUMN_WIDTH = 15;
 
 	// max returns the largest item in data
 	//
+
+	template <class T>
+
+	T max(const T* data, int n) {
+
+		T maxData = SMALLEST_NUMBER;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (maxData < data[i])
+			{
+				maxData = data[i];
+			}
+		}
+		return maxData;
+	}
 
 
 	// min returns the smallest item in data
 	//
 
-        
+	template <class T>
+
+	T min(const T* data, int n) {
+
+		T minData = LARGEST_NUMBER;
+
+		for (int i = 0; i < n; i++)
+		{
+			if (minData > data[i])
+			{
+				minData = data[i];
+			}
+		}
+		return minData;
+	}
+
+
 	// sum returns the sum of n items in data
 	//    
-   
+
+	template <class T>
+	T sum(const T* data, int n) {
+		T sumData = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			sumData += data[i];
+		}
+		return sumData;
+	}
+
 	// average returns the average of n items in data
 	//
+
+	template <class T>
+
+	double average(const T* data, int n) {
+
+		return (sum(data, n) / n);
+
+	}
 
 	// read extracts n items from input and stores them in data
 	// - the items are comma separated
@@ -33,11 +96,42 @@ namespace sict {
 	// - for example: ,data1, data2,data3, ...., datan
 	//
 
+	template <class T>
+
+	bool read(std::istream& input, T* data, int n) {
+	
+		for (int i = 0; i < n; i++)
+		{
+			input.ignore(256, ',');
+			input >> data[i];
+		}
+
+		return true;
+	}
+
+
 	// display inserts n items of data into std::cout
 	//
+
+	template <class T>
+
+	void display(const char* name, const T* data, int n) {
+	
+		std::cout.width(20);
+		std::cout << std::right << name;
+
+		for (int i = 0; i < n; i++)
+		{
+			std::cout.width(15);
+			std::cout << data[i];
+		}
+		std::cout << std::endl;
+	}
+
 
 	bool readRow(std::istream& input, const char* name, int* data, int n);
 	bool readRow(std::istream& input, const char* name, double* data, int n);
 	void answers(const int*, const int*, const int*, const double*, const int*, const int n);
 }
 
+#endif // !SICT_DATA_H
