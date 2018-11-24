d5556adb4c6ff09831f46cb8a4e183bf58f1e9c0
Kashyap Bharda <kashyapbharda12645@gmail.com>
2018-11-23 19:42:25 -0500

    W9 Done

--- WS09/in_lab/Data.cpp
+++ WS09/in_lab/Data.cpp
@@ -9,75 +9,7 @@
 using namespace std;
 
 namespace sict {
-	// readRow extracts a single record from input and stores n data fields in data
-	// - includes error checking: checks if the name of the row is valid.
-	//
-	bool readRow(std::istream& input, const char* name, double* data, int n) {
-		char rowname[2000];
-		bool ok = !input.bad();
-
-		// row format is 
-		// name,1,2,3,4,5,6
-
-		if (ok) {
-			// read the name  first
-
-			input.get(rowname, 2000, ',');
-			// note: get will read a string upto the , character
-			// it will leave the , character in the input buffer 
-
-			// check that the caller is reading the correct row (name)
-			// - if the user is reading the wrong row, return an error
-			if (std::strcmp(rowname, name) != 0) {
-				// wrong row
-				std::cout << "Cannot parse row for " << name << std::endl;
-				input.ignore(2000, '\n');
-				ok = false;
-			}
-		}
-
-		if (ok)
-			// read the data array
-			ok = read(input, data, n);
-
-		// finish the line
-		input.ignore(2000, '\n');
-		return ok;
-	}
-
-	bool readRow(std::istream& input, const char* name, int* data, int n) {
-		char rowname[2000];
-		bool ok = !input.bad();
-
-		// row format is 
-		// name,1,2,3,4,5,6
-
-		if (ok) {
-			// read the name  first
-
-			input.get(rowname, 2000, ',');
-			// note: get will read a string upto the , character
-			// it will leave the , character in the input buffer 
-
-			// check that the caller is reading the correct row (name)
-			// - if the user is reading the wrong row, return an error
-			if (std::strcmp(rowname, name) != 0) {
-				// wrong row
-				std::cout << "Cannot parse row for " << name << std::endl;
-				input.ignore(2000, '\n');
-				ok = false;
-			}
-		}
-
-		if (ok)
-			// read the data array
-			ok = read(input, data, n);
-
-		// finish the line
-		input.ignore(2000, '\n');
-		return ok;
-	}
-
+	
 	// answers outputs statistics for visual validation of data
 	//
 	void answers(const int* year, const int* population, const int* violentCrime,
--- WS09/in_lab/Data.h
+++ WS09/in_lab/Data.h
@@ -128,9 +128,40 @@ namespace sict {
 		std::cout << std::endl;
 	}
 
+	template <class T>
+	bool readRow(std::istream& input, const char* name, T* data, int n) {
+		char rowname[2000];
+		bool ok = !input.bad();
 
-	bool readRow(std::istream& input, const char* name, int* data, int n);
-	bool readRow(std::istream& input, const char* name, double* data, int n);
+		// row format is 
+		// name,1,2,3,4,5,6
+
+		if (ok) {
+			// read the name  first
+
+			input.get(rowname, 2000, ',');
+			// note: get will read a string upto the , character
+			// it will leave the , character in the input buffer 
+
+			// check that the caller is reading the correct row (name)
+			// - if the user is reading the wrong row, return an error
+			if (std::strcmp(rowname, name) != 0) {
+				// wrong row
+				std::cout << "Cannot parse row for " << name << std::endl;
+				input.ignore(2000, '\n');
+				ok = false;
+			}
+		}
+
+		if (ok)
+			// read the data array
+			ok = read(input, data, n);
+
+		// finish the line
+		input.ignore(2000, '\n');
+		return ok;
+	}
+	
 	void answers(const int*, const int*, const int*, const double*, const int*, const int n);
 }
 
